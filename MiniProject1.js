16375faa3b0f45d47ae3937c7b36e5932ddb9bc7
Adam Clements <aj.clements@live.co.uk>
2016-06-14 16:17:14 +0100

    Guess game with CSS

--- MiniProject1.js
+++ MiniProject1.js
@@ -1,13 +1,23 @@
 //Build a small game where the user has to guess a secret number which is generated automatically in Javascript. After each guess the webpage displays to the user whether their guess is too large or too small. When the user guesses the secret number correctly - three pieces of information are displayed on the webpage: (1) the correct number  (2)the number of guesses (3)the message “Well done – correct guess!”
 
-//button click
+//button click to guess number
 var event1 = document.getElementById("button1");
 event1.addEventListener('click', guess, false);
 
-//secret number use will guess
-var secretNum = Math.floor((Math.random() * 100) + 1);
+//reset button to start again
+var event2 = document.getElementById("reset");
+event2.addEventListener('click', reset1, false);
 
 var guesses = 0;
+
+var secretNum = Math.floor((Math.random() * 100) + 1);
+
+//secret number use will guess
+//function to reset game
+function reset1() {
+window.secretNum = Math.floor((Math.random() * 100) + 1);
+window.guesses = 0;
+}
 
 function guess(){
     //After every attempt, guesses increments 1
